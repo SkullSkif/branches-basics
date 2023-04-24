e8bf9a8e6ec50e4a83e4dfc7d005f961b2dd629a
SkullSkif <likvlad662@gmail.com>
2023-04-25 03:42:41 +0700

    Done output for math

--- main.c
+++ main.c
@@ -3,9 +3,10 @@
 int main()
 {
     int s,a,b;
-    printf("Hello, World!");
+    printf("Hello, World!\n");
     a = 4;
     b = 10;
     s = a + b;
+    printf("4 + 10 equals = %d\n",s);
     return 0;
 }
